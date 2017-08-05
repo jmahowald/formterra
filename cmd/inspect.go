9b86e70e2d461b813a2c4cd34b1b012eb16d9c32
Josh Mahowald <joshua.mahowald@gmail.com>
2017-08-04 21:31:50 -0500

    dummy commit to force travis into doing something

--- cmd/inspect.go
+++ cmd/inspect.go
@@ -17,7 +17,7 @@ package cmd
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/ghodss/yaml"
-	tf "github.com/backpackhealth/formterra/tfproject"
+	tf "github.com/jmahowald/formterra/tfproject"
 	"github.com/spf13/cobra"
 )
 
